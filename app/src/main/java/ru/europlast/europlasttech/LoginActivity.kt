ab9459c4d480ec7b5ba10517b14d76cbf2b678ff
yudin <brawlakkum@gmail.com>
2024-05-21 09:23:19 +0300

    complete Login screen

--- app/src/main/java/ru/europlast/europlasttech/LoginActivity.kt
+++ app/src/main/java/ru/europlast/europlasttech/LoginActivity.kt
@@ -1,10 +1,13 @@
 package ru.europlast.europlasttech
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,9 +16,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Switch
+import androidx.compose.material3.SwitchColors
+import androidx.compose.material3.SwitchDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
@@ -30,31 +38,34 @@ import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import ru.europlast.europlasttech.ui.theme.EuroplastTechTheme
+import ru.europlast.europlasttech.ui.theme.EvpCyan
+import ru.europlast.europlasttech.ui.theme.ThumbChecked
+import ru.europlast.europlasttech.ui.theme.ThumbUnchecked
+import ru.europlast.europlasttech.ui.theme.TrackChecked
+import ru.europlast.europlasttech.ui.theme.TrackUnchecked
+import ru.europlast.europlasttech.ui.theme.WhiteTransparent
+import kotlin.math.log
 
 class LoginActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            EuroplastTechTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            LoginScreen()
         }
     }
 }
-
+@Preview
 @Composable
 fun LoginScreen() {
     Box(
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+
     ) {
         Image(
             painter = painterResource(id = R.drawable.ic_login),
@@ -65,14 +76,16 @@ fun LoginScreen() {
 
         Column(
             modifier = Modifier
-                .padding(16.dp)
+                .padding(16.dp),
         ) {
             Text(
                 text = stringResource(id = R.string.authorization_label),
                 fontSize = 36.sp,
                 fontWeight = FontWeight.Bold,
                 textAlign = TextAlign.Center,
-                color = Color.White
+                color = Color.White,
+                modifier = Modifier.fillMaxWidth()
+
             )
 
             Spacer(modifier = Modifier.height(16.dp))
@@ -83,15 +96,21 @@ fun LoginScreen() {
             Column {
                 Text(
                     text = stringResource(id = R.string.login_label),
-
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 25.dp)
                     )
                 OutlinedTextField(
                     value = login.value,
                     onValueChange = { login.value = it },
-                    label = { },
-                    textStyle = TextStyle(color = Color.White),
-                    modifier = Modifier.fillMaxWidth()
+                    textStyle = TextStyle(color = Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp)
+                        .background(WhiteTransparent, shape = RoundedCornerShape(4.dp))
+                        .border(2.dp, Color.Gray, shape = RoundedCornerShape(4.dp))
                 )
+
             }
 
             Spacer(modifier = Modifier.height(8.dp))
@@ -102,15 +121,20 @@ fun LoginScreen() {
             Column {
                 Text(
                     text = stringResource(id = R.string.password_label),
+                    fontSize = 20.sp,
+                    color = Color.White,
+                    modifier = Modifier.padding(start = 25.dp)
 
                     )
                 OutlinedTextField(
                     value = password.value,
                     onValueChange = { password.value = it },
-                    label = { },
-                    textStyle = TextStyle(color = Color.White),
-                    visualTransformation = PasswordVisualTransformation(),
-                    modifier = Modifier.fillMaxWidth()
+                    textStyle = TextStyle(color = Color.Black),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp)
+                        .background(WhiteTransparent, shape = RoundedCornerShape(4.dp))
+                        .border(2.dp, Color.Gray, shape = RoundedCornerShape(4.dp))
                 )
             }
 
@@ -120,8 +144,19 @@ fun LoginScreen() {
             val isRememberMeClicked = remember { mutableStateOf(false) }
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Switch(
+
                     checked = isRememberMeClicked.value,
-                    onCheckedChange = { /* Handle switch state change */ }
+                    onCheckedChange = {
+                        isRememberMeClicked.value = !isRememberMeClicked.value},
+                    modifier = Modifier.padding(start = 20.dp),
+                    colors = SwitchDefaults.colors(
+                        checkedThumbColor = ThumbChecked,
+                        checkedTrackColor = TrackChecked,
+                        uncheckedThumbColor = ThumbUnchecked,
+                        uncheckedTrackColor = TrackUnchecked,
+                    )
+
+
                 )
 
                 Text(
@@ -131,7 +166,25 @@ fun LoginScreen() {
                     modifier = Modifier.padding(start = 8.dp)
                 )
             }
+            Spacer(modifier = Modifier.height(80.dp))
+
+            Button(onClick = { println("click") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(start = 20.dp, end = 20.dp),
+                colors = ButtonDefaults.buttonColors(EvpCyan)
+            ) {
+                Text(text = stringResource(R.string.auth_button_text),
+                    style = TextStyle(color = Color.White,
+                        fontSize = 24.sp),
+                    textAlign = TextAlign.Center
+               )
+
+                }
+            }
+
         }
+
     }
-}
+
 
--- app/src/main/java/ru/europlast/europlasttech/ui/theme/Color.kt
+++ app/src/main/java/ru/europlast/europlasttech/ui/theme/Color.kt
@@ -9,3 +9,11 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+val WhiteTransparent = Color(0xB3FFFFFF)
+val EvpCyan = Color(0xFF00A1D3)
+// SwitchColors //
+val ThumbChecked = Color(0xFF11FF00)
+val TrackChecked = Color(0xFFA5FF93)
+val ThumbUnchecked = Color(0xFFFF0000)
+val TrackUnchecked = Color(0xFFFFFFFF)
+
